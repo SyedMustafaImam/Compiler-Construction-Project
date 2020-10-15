d570353bc98596e508cd5437d0386932e67d888c
Syed Mustafa Imam <cs1812134@szabist.pk>
2020-10-15 22:40:11 +0500

    step2

--- temp/src/ccproject/main.java
+++ temp/src/ccproject/main.java
@@ -2,7 +2,6 @@ package temp;
 
 import java.util.*;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -11,58 +10,66 @@ import java.io.IOException;
 
 public class main {
     static File file = new File("test.txt");
-    public static void main(String[] args)  {
 
-    System.out.println("\n*************Welcome to Text Reader**********\n\n");   
-    System.out.println("Creating or Finding File...\n");
-    for(float i=0; i<=10;i++){
-    for(float j=0; j<=10; j++){		    
-		    
-		    j=j+1;
-    }}
-        try{
-        
-        if(file.createNewFile()){
-            System.out.println("File Created"+ file.getName());
+    public static void main(String[] args) {
 
-        }else{
-            System.out.println("File already exist.");
+        System.out.println("\n*************Welcome to Text Reader**********\n\n");
+        System.out.println("Creating or Finding File...\n");
+        for (float i = 0; i <= 10; i++) {
+            for (float j = 0; j <= 10; j++) {
+
+                j = j + 1;
+            }
+        }
+        try {
+
+            if (file.createNewFile()) {
+                System.out.println("File Created" + file.getName());
+
+            } else {
+                System.out.println("File already exist.");
+            }
+
+        } catch (IOException e) {
+            System.out.print("Got into an error!");
+            e.printStackTrace();
         }
 
-
-    }catch (IOException e){
-        System.out.print("Got into an error!");
-        e.printStackTrace();
-    }
-
-    readFromFile();
+        readFromFile();
 
     }
 
+    public static void readFromFile() {
 
-
-    public static void readFromFile(){
-       
-       System.out.println("\n---------------Reading Input Form File-------------------\n\nOutput:\n");
-       
-        
+        System.out.println("\n---------------Reading Input Form File-------------------\n\nOutput:\n");
+        System.out.println("\n---------------Ignoring Comments-------------------\n\nOutput:\n");
 
         try {
-        Scanner reader = new Scanner(file);
-        while(reader.hasNextLine()){
-            int i= 0;
-            String  data = reader.nextLine();
-    for(i=0; i<data.length();i++){
-        System.out.println(data.charAt(i));
+            Scanner reader = new Scanner(file);
+            
+            int i = 0;
+            String data = reader.nextLine();
+            for (i = 0; i < data.length(); i++) {
 
+                if (data.charAt(i) == '/') {
 
-    }        
-        }  
+                    i++;
+                }
+                if (data.charAt(i) == '*') {
+                    i++;
+                    while (data.charAt(i) == '*' && data.charAt(i) == '/') {
+                        i++;
+                    }
 
-        
-       } catch (FileNotFoundException e) {
-           System.out.println("Error encounterd!");
+                }
+
+                System.out.println(data.charAt(i));
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error encounterd!");
+        }
+
     }
-    
 }
-    }
